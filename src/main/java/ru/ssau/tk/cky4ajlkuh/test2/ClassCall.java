7f8a14cb78766c5c54f409cb399abc4400019bc4
Konstantinov <misha.tank.2014@mail.ru>
2020-09-08 23:38:27 +0400

    Создание точек и вывод

---
+++ src/main/java/ru/ssau/tk/cky4ajlkuh/test2/ClassCall.java
@@ -0,0 +1,13 @@
+package ru.ssau.tk.cky4ajlkuh.test2;
+
+public class ClassCall {
+
+    public static void main(String[] args) {
+        ClassPoint point_1 = new ClassPoint(1,2,3);
+        ClassPoint point_2 = new ClassPoint(64,32,45);
+        ClassPoint point_3 = new ClassPoint(41,90,100);
+        point_1.Points();
+        point_2.Points();
+        point_3.Points();
+    }
+}
