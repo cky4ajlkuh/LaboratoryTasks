2b1eed70e8230615f0e212a3a9b385e11e2d8bf7
Konstantinov <misha.tank.2014@mail.ru>
2020-09-09 12:31:40 +0400

    исправление ошибок

--- src/main/java/ru/ssau/tk/cky4ajlkuh/test2/ClassCall.java
+++ src/main/java/ru/ssau/tk/cky4ajlkuh/test2/ClassCall.java
@@ -3,11 +3,11 @@ package ru.ssau.tk.cky4ajlkuh.test2;
 public class ClassCall {
 
     public static void main(String[] args) {
-        ClassPoint point_1 = new ClassPoint(1,2,3);
-        ClassPoint point_2 = new ClassPoint(64,32,45);
-        ClassPoint point_3 = new ClassPoint(41,90,100);
-        point_1.Points();
-        point_2.Points();
-        point_3.Points();
+        ClassPoint red = new ClassPoint(255, 0, 0);
+        ClassPoint blue = new ClassPoint(0, 0, 255);
+        ClassPoint green = new ClassPoint(0, 255, 0);
+        red.points();
+        blue.points();
+        green.points();
     }
 }
--- src/main/java/ru/ssau/tk/cky4ajlkuh/test2/ClassPoint.java
+++ src/main/java/ru/ssau/tk/cky4ajlkuh/test2/ClassPoint.java
@@ -6,16 +6,16 @@ public class ClassPoint {
     public final double y;
     public final double z;
 
-    public ClassPoint(double x, double y,double z) {
-
-        this.x=x;
-        this.y=y;
-        this.z=z;
+    public ClassPoint(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
-    public void Points() {
 
+    public void points() {
         System.out.print(x);
         System.out.print(y);
         System.out.print(z);
+        System.out.println(" ");
     }
 }
