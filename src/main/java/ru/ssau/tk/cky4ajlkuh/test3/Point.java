4e2c6cdd76dcde5c1f13ca39b16b070f05c547ef
Konstantinov <misha.tank.2014@mail.ru>
2020-09-16 12:41:56 +0400

    Создан класс точки

---
+++ src/main/java/ru/ssau/tk/cky4ajlkuh/test3/Point.java
@@ -0,0 +1,23 @@
+package ru.ssau.tk.cky4ajlkuh.test3;
+
+public class Point {
+
+    public double x;
+    public double y;
+    public double z;
+
+    public Point (double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    public double getX() {
+        return x;
+    }
+    public double getY() {
+        return y;
+    }
+    public double getZ() {
+        return z;
+    }
+}
