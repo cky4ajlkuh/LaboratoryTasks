f33c1a1e3a55c35607facb177ad12365b6052341
Konstantinov <misha.tank.2014@mail.ru>
2021-01-03 03:47:52 +0400

    5.3 не дописал итератор немного и тесты еще пока не сделал

--- src/main/java/ru/ssau/tk/cky4ajlkuh/create/POJO/Route.java
+++ src/main/java/ru/ssau/tk/cky4ajlkuh/create/POJO/Route.java
@@ -1,9 +1,14 @@
 package ru.ssau.tk.cky4ajlkuh.create.POJO;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.NoSuchElementException;
 
-public class Route {
+import static jdk.nashorn.internal.objects.NativeArray.lastIndexOf;
+
+public class Route implements Iterable<Location> {
     List<Location> locations;
+
     public Route(List<Location> locations) {
         this.locations = locations;
     }
@@ -11,13 +16,46 @@ public class Route {
     public List<Location> getLocations() {
         return locations;
     }
-    public void addLocationOfIndex(Location location, int index){
-        locations.add(index,location);
+
+    public void addLocationOfIndex(Location location, int index) {
+        locations.add(index, location);
     }
-    public void addLocation(Location location){
+
+    public void addLocation(Location location) {
         locations.add(location);
     }
-    public void removeLocation(int index){
+
+    public void removeLocation(int index) {
         locations.remove(index);
+    }
+
+    public void getFirstLocation() {
+        locations.get(0);
+    }
+
+    public void getLastLocation() {
+        locations.get((int) lastIndexOf(locations));
+    }
+
+    @Override
+    public Iterator<Location> iterator() {
+        return new Iterator<>() {
+            int i = 0;
+
+            @Override
+            public boolean hasNext() {
+                return i < locations.size();
+            }
+
+            @Override
+            public Location next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                Location location = new Location();
+                i++;
+                return location;
+            }
+        };
     }
 }
